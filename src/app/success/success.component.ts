ca8270430cded6199dd99d0603976a6a76aa7bee
Anton <antonoliverkiiski@gmail.com>
2023-01-11 13:08:03 +0200

    Add styling and new font to app

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-success',
   templateUrl: './success.component.html',
-  styleUrls: ['./success.component.css']
+  styleUrls: ['./success.component.css'],
 })
 export class SuccessComponent {
-
+  faIcon = faThumbsUp;
 }
--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faSkullCrossbones } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-warning',
   templateUrl: './warning.component.html',
-  styleUrls: ['./warning.component.css']
+  styleUrls: ['./warning.component.css'],
 })
 export class WarningComponent {
-
+  faIcon = faSkullCrossbones;
 }
